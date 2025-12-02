cd301ed75ab7ee74dfc5c2b9e46b050712a29f23
Stefan Vujadinovic <59982660+vujadinovic@users.noreply.github.com>
2025-12-02 17:41:11 +0100

    Added adaline and fixed perceptron

--- adaline.py
+++ adaline.py
@@ -1,9 +1,10 @@
 import numpy as np
 class Perceptron:
     def __init__(self, l_rate = 0.01, 
-                 n_iter = 50, random_state = 1):
+                 n_iter = 50, shuffle = True, random_state = 1):
         self.l_rate = l_rate
         self.n_iter = n_iter
+        self.shuffle = shuffle
         self.random_state = random_state
 
     def fit(self, X, y):
@@ -11,20 +12,23 @@ class Perceptron:
         self.w_ = rgen.normal(loc=0.0, scale=0.01,
                               size=X.shape[1])
         self.b_ = np.float(0.)
-        self.errors_ = []
-        for _ in range(self.n_iter):
-            errors = 0
-            for xi, target in zip(X, y):
-                update = self.l_rate * (target - self.predict(xi))
-                self.w_ += update * xi
-                self.b_ += update
-                errors += int(update != 0.0)
-            self.errors_.append(errors)
+        self.losses_ = []
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y - output)
+            self.w_ += self.l_rate * 2.0 * X.T.dot(errors) / X.shape[0]
+            self.b_ += self.l_rate * 2.0 * errors.mean()
+            loss = (errors**2).mean()
+            self.losses_.append(loss)
         return self
     
     def net_input(self, X):
         return np.dot(X, self.w_) + self.b_
     
+    def activation(self, X):
+        return X
+
     def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, 0)
     
